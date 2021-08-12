56e5e400b063ee0cec4672b2d80592cd5a5b3fd7
Davids Chijioke Prince <77537310+cj-prince@users.noreply.github.com>
2021-08-12 15:13:56 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let count = 0;
+let Display = document.getElementById('num') ;
+let countIncrease = document.querySelector('.btn-increase');
+let countDecrease = document.querySelector('.btn-decrease');
+let countReset = document.querySelector('.btn-reset');
+
+
+countDisplay();
+
+countIncrease.addEventListener("click", ()=>{
+    count++;
+    countDisplay()
+    
+} );
+
+countDecrease.addEventListener("click", ()=>{
+    count--;
+   countDisplay()
+});
+
+countReset.addEventListener("click", ()=>{
+    count = 0
+    countDisplay()
+})
+
+
+
+// function updateDisplay(){
+//     countDisplay.textContent = count;
+// };
+function countDisplay(){
+    Display.innerHTML = count
+    if(count > 0) {
+        Display.style.color = 'green'
+    }
+     if(count < 0) {
+        Display.style.color = 'red'
+    }
+     if(count === 0) {
+        Display.style.color = '#222'
+    }
+}
